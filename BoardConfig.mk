ccfe520e069f66fb5e76dc60a2db3059b601c984
mlm-games <125530737+mlm-games@users.noreply.github.com>
2024-12-11 15:01:05 +0530

    reduce more size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,8 @@ TW_EXCLUDE_LPDUMP := true
 TW_EXCLUDE_LPTOOLS := true
 TW_EXCLUDE_NANO := true
 TW_EXCLUDE_BASH := true
+TW_EXCLUDE_PYTHON := true
+TW_EXCLUDE_TWRPAPP := true
 
 # Auto configures these
 #TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
